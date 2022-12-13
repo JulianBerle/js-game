0ddc607640f1fde0c9c970771bece6aad46edfe3
Julian Berle <berlejulian@gmail.com>
2022-12-13 21:20:10 +0100

    Added for in loop in function moveInvaders

--- js/script.js
+++ js/script.js
@@ -52,8 +52,11 @@ function moveInvaders() {
     const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1;
     remove();
 
-    
 
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += 1;
+    }
 
+    draw();
 }
 
